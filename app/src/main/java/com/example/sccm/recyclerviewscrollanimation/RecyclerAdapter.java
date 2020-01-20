dc2a7d85d8937059a2be012cb2ae3e544bac0413
user <g.brahmadatta@gmail.com>
2020-01-20 16:24:20 +0530

    added enter animation

--- app/src/main/java/com/example/sccm/recyclerviewscrollanimation/RecyclerAdapter.java
+++ app/src/main/java/com/example/sccm/recyclerviewscrollanimation/RecyclerAdapter.java
@@ -1,15 +1,19 @@
 package com.example.sccm.recyclerviewscrollanimation;
 
 import android.content.Context;
+import android.os.Build;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -31,6 +35,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Recycl
         return new RecyclerViewHolder(view);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     public void onBindViewHolder(@NonNull RecyclerViewHolder holder, int position) {
 
@@ -38,6 +43,9 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Recycl
 
         holder.imageView.setAnimation(AnimationUtils.loadAnimation(mContext,R.anim.fade_transititon));
         holder.container.setAnimation(AnimationUtils.loadAnimation(mContext,R.anim.fade_scale_animation));
+
+        runEnterAnimation(holder.itemView);
+
 
         holder.imageView.setImageResource(mRecyclerModels.get(position).image);
         holder.headline.setText(mRecyclerModels.get(position).headline);
@@ -49,6 +57,18 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Recycl
         holder.itemView.startAnimation(animation);*/
         lastPosition = position;
 
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private void runEnterAnimation(View view) {
+        Display display = view.getDisplay();
+
+        view.setTranslationY(500);
+        view.animate()
+                .translationY(0)
+                .setInterpolator(new DecelerateInterpolator(3.f))
+                .setDuration(700)
+                .start();
     }
 
     @Override
